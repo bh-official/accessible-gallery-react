358e9f9872291ec1c6b6bed8a3c6bc09870314e2
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-13 14:57:22 +0000

    added loaded set loaded to keep the image till new image arrives

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,12 +1,36 @@
+// export default function Viewer({ image, isFading }) {
+//   return (
+//     <div>
+//       <img
+//         key={image.src}
+//         src={image.src}
+//         srcSet={image.srcset}
+//         alt={image.alt}
+//         className={`viewer-image ${isFading ? "fade-out" : "fade-in"}`}
+//       />
+
+//       <div className="gallery-caption">{image.caption}</div>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+
 export default function Viewer({ image, isFading }) {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <div>
+    <div className="viewer-wrapper">
+      {!loaded && <div className="image-loader">Loading image...</div>}
+
       <img
-        key={image.src}
         src={image.src}
         srcSet={image.srcset}
         alt={image.alt}
-        className={`viewer-image ${isFading ? "fade-out" : "fade-in"}`}
+        onLoad={() => setLoaded(true)}
+        className={`viewer-image ${loaded ? "visible" : "hidden"} ${
+          isFading ? "fade-out" : "fade-in"
+        }`}
       />
 
       <div className="gallery-caption">{image.caption}</div>
